639f81b067d0b3d27cc1d6338b9cb0253371e359
Graham Miles <ghmiles328@gmail.com>
2025-02-19 00:08:20 -0600

    i hate docker

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,17 +1,14 @@
 // @ts-check
-import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
-
   vite: {
     plugins: [tailwindcss(), tsConfigPaths()],
   },
+
+  integrations: [react()],
 })
