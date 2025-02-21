88c9d3c4387614fbea3e770f0708f7d52a942a04
Graham Miles <ghmiles328@gmail.com>
2025-02-21 00:45:28 -0600

    vite with bun

--- apps/vite-admin/vite.config.ts
+++ apps/vite-admin/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -5,4 +6,12 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsConfigPaths()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    copyPublicDir: true,
+  },
 })
