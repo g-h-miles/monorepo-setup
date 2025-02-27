acc28b50fc5e7784bd1e17795f14335a6d71393b
Graham Miles <ghmiles328@gmail.com>
2025-02-27 01:50:14 -0600

    tnast

--- apps/tanstack/app/utils/users.tsx
+++ apps/tanstack/app/utils/users.tsx
@@ -4,4 +4,7 @@ export type User = {
   email: string
 }
 
-export const DEPLOY_URL = 'http://localhost:3004'
+const PROD_URL = import.meta.env.PROD_URL || 'https://tanstack.mlcr.us'
+export const DEPLOY_URL = import.meta.env.PROD
+  ? PROD_URL
+  : 'http://localhost:3004'
